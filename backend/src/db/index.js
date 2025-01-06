14d2bb1d58ba3fcf8b1a1f9ea573690504ef9c26
SandeepSwain <sandeepswain2004@gmail.com>
2025-01-07 01:41:26 +0530

    DB connection

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -5,10 +5,9 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
     try {
-        // const connectionInstance = await mongoose.connect(
-        //     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${DB_NAME}`
-        // );
-        const connectionInstance = await mongoose.connect(`mongodb+srv://tirthratanpara454:tirth123@tirth.1w8atvn.mongodb.net/${DB_NAME}`)
+        const connectionInstance = await mongoose.connect(
+            `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${DB_NAME}`
+        );
         logger.info(
             chalk.blue.bold.italic(`connected to MongoDB,DB_host: ${connectionInstance.connection.host}`)
         );
